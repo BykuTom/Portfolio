63184e573599fd74474c686be2b73291a7a5d923
Tomasz Wolak <tom@outlook.com>
2023-05-29 10:18:00 +0100

    On branch feature  Changes to be committed: 	modified:   CSS/index.css 	new file:   IMG/Forest.jpg 	new file:   IMG/blueberries-3440x1440.jpg 	new file:   IMG/portfolio/Website-Example.png 	modified:   JS/index.js 	new file:   JS/isotope.pkgd.min.js 	modified:   index.html
    Added Jquery liblary and isotope plugin to create the poftfolio,
    
    Added a section observer to create the records section,
    
    Added a canvas and an animated background net effect,
    
    Changed the look of the website including buttons and colour scheme,

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,13 @@
+/*Jquery Isotope*/
+
+$(".grid").isotope({
+  layoutMode: "fitRows",
+  itemSelector: ".grid-item",
+  transitionDuration: "0.6s",
+});
+
+/*End of Jquery Isotope*/
+
 /*Type Writer*? */
 const animatedText = document.querySelector(".end-text");
 const blinker = document.querySelector(".blinker");
@@ -39,9 +49,7 @@ typeWriter = () => {
 textRemover = () => {
   if (blinker.style.animationPlayState === "running") {
     blinker.style.animationPlayState = "paused";
-    console.log(blinker.style.opacity);
     blinker.style.opacity = "1";
-    console.log(blinker.style.opacity);
   }
   strings = animatedText.textContent.slice(0, textLenght);
   animatedText.textContent = strings;
@@ -57,71 +65,222 @@ textRemover = () => {
 };
 /*End of typewriter */
 /* Section Observer*/
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {});
-});
+const record_wrap = document.querySelector(".records");
+const records_numbers = document.querySelectorAll(".number");
+const record_Circles = document.querySelectorAll(".record-circle");
 
+record_Circles.forEach((circle) => circle.classList.remove("active"));
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("visibile");
+        records_numbers.forEach((number) => {
+          const UpdateCount = () => {
+            let currentNumber = Number(number.innerText);
+            let maximumNumber = Number(number.dataset.number);
+            const increment = Math.ceil(Math.sqrt(maximumNumber) / 15);
+            if (currentNumber < maximumNumber) {
+              number.innerText = currentNumber + increment;
+              setTimeout(UpdateCount, 1);
+            } else {
+              number.innerText = maximumNumber;
+            }
+          };
+          record_Circles.forEach((circle) => circle.classList.add("active"));
+          setTimeout(UpdateCount, 200);
+        });
+      }
+    });
+  },
+  { threshold: 0.7 }
+);
+
+const config = {
+  root: null, // Use the viewport as the root element
+  threshold: 1, // Trigger the observer callback when the element is 50% visible
+};
+observer.root = null;
+observer.threshold = 1;
+observer.observe(record_wrap);
+
+RecordsCounter = () => {
+  records_number.forEach();
+
+  animatedText.textContent = typeWriterText[arrayIndex].substring(0, textIndex);
+
+  if (textIndex++ != typeWriterText[arrayIndex].length) {
+    setTimeout(typeWriter, delay);
+  } else {
+    blinker.style.animationPlayState = "running";
+    textLenght = typeWriterText[arrayIndex].length + 1;
+    setTimeout(textRemover, 3000);
+  }
+};
 /*End of section Onserver */
 /* header background*/
-const canvas = document.querySelector(".header-background");
-const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-context.fillStyle = "#00a7e1";
-class Metaball {
-  constructor(effect) {
-    this.effect = effect;
-    this.x = this.effect.width * 0.5;
-    this.y = this.effect.height * 0.5;
-    this.radius = Math.random() * 200 + 50;
-    this.speedX = Math.random() - 0.5;
-    this.SpeedY = Math.random() - 0.5;
-  }
-  update() {
-    if (this.x < this.radius || this.x > this.effect.width - this.radius)
-      this.speedX *= -1;
-    if (this.y < this.radius || this.y > this.effect.height - this.radius)
-      this.SpeedY *= -1;
-    this.x += this.speedX;
-    this.y += this.SpeedY;
-  }
-  draw(context) {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fill();
-  }
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+
+let w, h, particles;
+let particleDistance = 64;
+let mouse = {
+  x: undefined,
+  y: undefined,
+  radius: 128,
+};
+
+function init() {
+  resizeReset();
+  animationLoop();
 }
-class MetaballsEffect {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.metaBallsArray = [];
-  }
-  init(numberOfBalls) {
-    for (let i = 0; i < numberOfBalls; i++) {
-      this.metaBallsArray.push(new Metaball(this));
+
+function resizeReset() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+  particles = [];
+  for (
+    let y =
+      (((h - particleDistance) % particleDistance) + particleDistance) / 2;
+    y < h;
+    y += particleDistance
+  ) {
+    for (
+      let x =
+        (((w - particleDistance) % particleDistance) + particleDistance) / 2;
+      x < w;
+      x += particleDistance
+    ) {
+      particles.push(new Particle(x, y));
     }
-  }
-  update() {
-    this.metaBallsArray.forEach((ball) => ball.update());
-  }
-  draw(context) {
-    this.metaBallsArray.forEach((ball) => ball.draw(context));
   }
 }
 
-const effect = new MetaballsEffect(canvas.width, canvas.height);
-effect.init(20);
-function animateMetaballs() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  effect.update();
-  effect.draw(context);
-  requestAnimationFrame(animateMetaballs);
+function animationLoop() {
+  ctx.clearRect(0, 0, w, h);
+  drawScene();
+  // console.log("im here");
+  setTimeout(animationLoop, 12);
 }
-animateMetaballs();
+
+function drawScene() {
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].draw();
+  }
+  drawLine();
+}
+
+function drawLine() {
+  for (let a = 0; a < particles.length; a++) {
+    for (let b = a; b < particles.length; b++) {
+      let dx = particles[a].x - particles[b].x;
+      let dy = particles[a].y - particles[b].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < particleDistance * 2) {
+        opacity = 1 - distance / (particleDistance * 1.25);
+        const gradient = ctx.createLinearGradient(0, 0, w, h);
+        gradient.addColorStop(0.16, "#4a4a4a64");
+        gradient.addColorStop(0.32, "#5a5a5a64");
+        gradient.addColorStop(0.5, "#fffdf796");
+        gradient.addColorStop(0.68, "#5a5a5a64");
+        gradient.addColorStop(0.84, "#4a4a4a64");
+        ctx.strokeStyle = gradient;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(particles[a].x, particles[a].y);
+        ctx.lineTo(particles[b].x, particles[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+function mousemove(e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+}
+
+function mouseout() {
+  mouse.x = undefined;
+  mouse.y = undefined;
+}
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 2;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.speed = Math.random() * 25 + 5;
+  }
+  draw() {
+    const gradient = ctx.createLinearGradient(0, 0, w, h);
+    gradient.addColorStop(0.16, "#3c3c3c64");
+    gradient.addColorStop(0.32, "#fffdf764");
+    gradient.addColorStop(0.68, "#fffdf764");
+    gradient.addColorStop(0.84, "#3c3c3c64");
+    ctx.fillStyle = gradient;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance; // 0 ~ 1
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let directionX = forceDirectionX * force * this.speed;
+    let directionY = forceDirectionY * force * this.speed;
+
+    if (distance < mouse.radius) {
+      this.x -= directionX;
+      this.y -= directionY;
+    } else {
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
+    }
+  }
+}
+
+init();
+window.addEventListener("resize", resizeReset);
+window.addEventListener("mousemove", mousemove);
+window.addEventListener("mouseout", mouseout);
+
 /*End of header background*/
 
 window.addEventListener("load", typeWriter);
+
+const filter_buttons = document.querySelectorAll(".filter-button");
+
+console.log(filter_buttons);
+
+filter_buttons.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    filter_buttons.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({
+      filter: filterValue,
+    });
+  })
+);
 
 /* const loadAnimatedText = () => {
   setTimeout(() => {
